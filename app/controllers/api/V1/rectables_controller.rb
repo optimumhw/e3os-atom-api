1243a0078091f1a7f597560918931043c0679f5f
Hal.Wilkinson <hal.wilkinson@optimumenergyco.com>
2018-05-29 08:40:19 -0700

    fixing

--- app/controllers/api/V1/rectables_controller.rb
+++ app/controllers/api/V1/rectables_controller.rb
@@ -12,32 +12,14 @@ module Api
 			end
 
 			def create
-
 				rectable = Rectable.create!( rectable_params )
-
-				p params
-				p rectable_params
-				p rectable
-
-				pp params["staging_order"]
-				pp params["staging_table"]
-
 				render status: :created, json: rectable.as_json
-
-				#
-				#if rectable.save
-				#	render json: {status: 'SUCCESS', message: 'rectable saved', data:rectable }, status: :ok
-				#else
-				#	render json: {status: 'ERROR', message: 'rectable not saved', 
-				#		data:rectable.errors }, status: :unprocessable_entity
-				#end
 			end
 
 			def destroy
 				rectable = Rectable.find(params[:id])
 				rectable.destroy
 				render json: {status: 'SUCCESS', message: 'Deleted rectable', data:rectable }, status: :ok
-
 			end
 
 			def update
@@ -48,15 +30,13 @@ module Api
 					render json: {status: 'ERROR', message: 'rectable not updated', 
 						data:rectable.errors }, status: :unprocessable_entity
 				end
-
 			end
 
 
 			private
 
 			def rectable_params
-				p "here i am !!!!!!!!!"
-				params.permit(:site_id, :number_chillers, :staging_order, :staging_table)
+				params.permit(:site_id, :number_chillers, :staging_order => [], :staging_table => [])
 			end
 
 		end
