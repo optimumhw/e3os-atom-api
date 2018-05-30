3c1fa1dee5920f4fad7916ec9039b65d981a81b0
Hal.Wilkinson <hal.wilkinson@optimumenergyco.com>
2018-05-30 09:22:43 -0700

    cleanup

--- app/models/rectable.rb
+++ app/models/rectable.rb
@@ -1,9 +1,5 @@
 class Rectable < ApplicationRecord
 	validates :site_id, presence: true
-
-	p "here...."
-	#rectable = Rectable.find_by(params[:site_id]).order(:created_at).take
 	scope :latest!, -> (site_id) { 
-		p site_id
 		where("rectables.site_id = ?", site_id).last! }
 end
